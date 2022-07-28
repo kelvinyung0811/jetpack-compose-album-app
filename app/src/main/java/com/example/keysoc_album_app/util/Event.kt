bc768593937fcebfb211439ea5ff0f715e74113f
Kelvin Yung <kelvinyung0811@gmail.com>
2022-07-28 15:55:41 +0800

    Add resource and event util class

---
+++ app/src/main/java/com/example/keysoc_album_app/util/Event.kt
@@ -0,0 +1,25 @@
+package com.example.keysoc_album_app.util
+
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again
+     */
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if its already handled
+     */
+    fun peakContent(): T = content
+}
